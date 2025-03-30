9e722a15f1d0939f4381aa433dcb43aa7e84c831
Liza <elapteva@golim.com>
2025-03-30 21:46:43 +0300

    section: newsletter

--- assets/global.js
+++ assets/global.js
@@ -1432,31 +1432,33 @@ document.addEventListener("DOMContentLoaded", function () {
 
 const slider = document.querySelector('.follow-slider');
 
-let isDown = false;
-let startX;
-let scrollLeft;
+if (slider) {
+  let isDown = false;
+  let startX;
+  let scrollLeft;
 
-slider.addEventListener('mousedown', (e) => {
-  isDown = true;
-  slider.classList.add('active');
-  startX = e.pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;
-});
+  slider.addEventListener('mousedown', (e) => {
+    isDown = true;
+    slider.classList.add('active');
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+  });
 
-slider.addEventListener('mouseleave', () => {
-  isDown = false;
-  slider.classList.remove('active');
-});
+  slider.addEventListener('mouseleave', () => {
+    isDown = false;
+    slider.classList.remove('active');
+  });
 
-slider.addEventListener('mouseup', () => {
-  isDown = false;
-  slider.classList.remove('active');
-});
+  slider.addEventListener('mouseup', () => {
+    isDown = false;
+    slider.classList.remove('active');
+  });
 
-slider.addEventListener('mousemove', (e) => {
-  if (!isDown) return;
-  e.preventDefault();
-  const x = e.pageX - slider.offsetLeft;
-  const walk = (x - startX) * 1.5; // scroll speed
-  slider.scrollLeft = scrollLeft - walk;
-});
+  slider.addEventListener('mousemove', (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 1.5; // scroll speed
+    slider.scrollLeft = scrollLeft - walk;
+  });
+}
